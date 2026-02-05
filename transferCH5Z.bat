33142f88e429b05b67c833f2d4362fd49adf15f7
Marco Priarone <mpriarone@gmail.com>
2026-02-05 10:44:14 +0100

    Add project files.

---
+++ transferCH5Z.bat
@@ -0,0 +1,56 @@
+@ECHO OFF
+REM *************************************************************************
+REM * Use -k on cUrl to bypass SSL certificate verification                 *
+REM * Use -v for call debugging                                             *
+REM *************************************************************************
+
+ECHO Starting VC4 API - Modify Program Library Entry CH5Z
+
+REM Load variables from settings.txt
+IF EXIST settings.txt (
+    FOR /F "tokens=1,2 delims==" %%A IN (settings.txt) DO (
+        SET %%A=%%B
+    )
+) ELSE (
+    ECHO Error: settings.txt not found!
+    PAUSE
+    EXIT /B
+)
+
+REM Get Authorizations groups
+REM curl -k -X GET "https://%IP%/VirtualControl/config/api/Authentication" -H "accept: application/json" -H "Authorization: %Token%"
+REM Reads all programs to get their ProgramID useful for oprogram operations
+curl -k -X GET "https://%IP%/VirtualControl/config/api/ProgramLibrary" -H "accept: application/json" -H "Authorization: %Token%"
+
+ECHO.
+ECHO.
+
+ECHO Modify Program Library Entry for ProgramID %ProgramID% with FriendlyName %FriendlyName% with new CH5Z
+REM Modify existing CH5
+REM Nota: Usiamo -F (form) invece di -d (data) per gestire correttamente il file e i campi multipart
+curl -k -X PUT "https://%IP%/VirtualControl/config/api/ProgramLibrary" ^
+     -H "accept: application/json" ^
+     -H "Authorization: %Token%" ^
+     -H "Content-Type: multipart/form-data" ^
+     -F "ProgramId=%ProgramID%" ^
+     -F "FriendlyName=%FriendlyName%" ^
+     -F "Notes=%Notes%" ^
+     -F "WebxPanelFile=@%WebxPanelFile%"
+
+ECHO.
+ECHO.
+ECHO Restarting
+curl -k -X GET "https://%IP%/VirtualControl/config/api/ProgramInstance"  ^
+ -H "accept: application/json"  ^
+ -H "Authorization: %Token%" 
+
+ECHO.
+ECHO.
+
+REM Restart Program to apply changes
+curl -k -X PUT "https://%IP%/VirtualControl/config/api/ProgramInstance" ^
+         -H "accept: application/json" -H "Authorization: %Token%" ^
+         -H "Content-Type: multipart/form-data" ^
+         -F "ProgramInstanceId=%ProgramInstanceId%" ^
+         -F "Name=%FriendlyName%" ^
+         -F "Restart=true"
---
+++ transferCPZ.bat
@@ -0,0 +1,55 @@
+@ECHO OFF
+REM *************************************************************************
+REM * Use -k on cUrl to bypass SSL certificate verification                *
+REM * Use -v for call debugging                                             *
+REM *************************************************************************
+ECHO Starting VC4 API - Modify Program Library Entry
+
+REM Load variables from settings.txt
+IF EXIST settings.txt (
+    FOR /F "tokens=1,2 delims==" %%A IN (settings.txt) DO (
+        SET %%A=%%B
+    )
+) ELSE (
+    ECHO Error: settings.txt not found!
+    PAUSE
+    EXIT /B
+)
+
+REM Get Authorizations groups
+REM curl -k -X GET "https://%IP%/VirtualControl/config/api/Authentication" -H "accept: application/json" -H "Authorization: %Token%"
+REM Reads all programs to get their ProgramID useful for program operations
+curl -k -X GET "https://%IP%/VirtualControl/config/api/ProgramLibrary" -H "accept: application/json" -H "Authorization: %Token%"
+
+ECHO.
+ECHO.
+
+ECHO Modify Program Library Entry for ProgramID %ProgramID% with FriendlyName %FriendlyName%
+REM Modify existing CPZ
+REM Note: We use -F (form) instead of -d (data) to correctly handle the file and multipart fields
+curl -k -X PUT "https://%IP%/VirtualControl/config/api/ProgramLibrary" ^
+     -H "accept: application/json" ^
+     -H "Authorization: %Token%" ^
+     -H "Content-Type: multipart/form-data" ^
+     -F "ProgramId=%ProgramID%" ^
+     -F "FriendlyName=%FriendlyName%" ^
+     -F "Notes=%Notes%" ^
+     -F "AppFile=@%AppFile%"
+
+ECHO.
+ECHO.
+ECHO Restarting
+curl -k -X GET "https://%IP%/VirtualControl/config/api/ProgramInstance"  ^
+ -H "accept: application/json"  ^
+ -H "Authorization: %Token%"  
+
+ECHO.
+ECHO.
+
+REM Restart Program to apply changes
+curl -k -X PUT "https://%IP%/VirtualControl/config/api/ProgramInstance" ^
+         -H "accept: application/json" -H "Authorization: %Token%" ^
+         -H "Content-Type: multipart/form-data" ^
+         -F "ProgramInstanceId=%ProgramInstanceId%" ^
+         -F "Name=%FriendlyName%" ^
+         -F "Restart=true"
